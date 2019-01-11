783e6fc987ad5fa3f33a25fc2747f8d2f07a6bc7
Viktor Doroshenko <viktordoroshenko@Viktors-MacBook-Air.local>
2019-01-11 15:10:13 +0200

    final

--- src/main/java/vms/vmsdispatcher/VmsDispatcherApplication.java
+++ src/main/java/vms/vmsdispatcher/VmsDispatcherApplication.java
@@ -2,12 +2,23 @@ package vms.vmsdispatcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
 
+@ManagedResource
 @SpringBootApplication
 public class VmsDispatcherApplication {
 
+	private static ConfigurableApplicationContext ctx;
+
 	public static void main(String[] args) {
-		SpringApplication.run(VmsDispatcherApplication.class, args);
+		ctx = SpringApplication.run(VmsDispatcherApplication.class, args);
+	}
+
+	@ManagedOperation
+	public static void stop() {
+		ctx.close();
 	}
 
 }
--- src/main/java/vms/vmsdispatcher/service/interfaces/ISensors.java
+++ src/main/java/vms/vmsdispatcher/service/interfaces/ISensors.java
@@ -3,5 +3,10 @@ package vms.vmsdispatcher.service.interfaces;
 import vms.vmsdispatcher.dto.SensorTypeEnum;
 
 public interface ISensors {
+  /**
+   * Returns sensor type depending on given sensor id;
+   * @param sensorId
+   * @return
+   */
   public SensorTypeEnum getSensorType(int sensorId);
 }
